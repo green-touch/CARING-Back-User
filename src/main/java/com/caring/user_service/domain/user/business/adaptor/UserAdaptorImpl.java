8cf08586dffe11f8cb24d3bb2e2b65232b9c0af4
hann <wjdgks1233@gmail.com>
2025-01-10 20:41:05 +0900

    [CARE-20] feature : create UserAdaptor & UserDomainService #3

--- src/main/java/com/caring/user_service/domain/user/business/adaptor/UserAdaptorImpl.java
+++ src/main/java/com/caring/user_service/domain/user/business/adaptor/UserAdaptorImpl.java
@@ -1,4 +1,25 @@
 package com.caring.user_service.domain.user.business.adaptor;
 
-public class UserAdaptorImpl {
+import com.caring.user_service.common.annotation.Adaptor;
+import com.caring.user_service.domain.user.entity.User;
+import com.caring.user_service.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@Adaptor
+@RequiredArgsConstructor
+public class UserAdaptorImpl implements UserAdaptor{
+
+    private final UserRepository userRepository;
+
+    @Override
+    public User findById(Long userId) {
+        return userRepository.findById(userId).orElseThrow(() -> new RuntimeException("not found user"));
+    }
+
+    @Override
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
 }
