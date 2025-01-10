61fc06a84b130bd5a138480066cbda2ddc76bbbe
hann <wjdgks1233@gmail.com>
2025-01-10 21:57:01 +0900

    [CARE-20] feature : create WebConfig.class #3

---
+++ src/main/java/com/caring/user_service/config/WebConfig.java
@@ -0,0 +1,105 @@
+package com.caring.user_service.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authorization.AuthorizationDecision;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
+
+@Slf4j
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class WebConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        configureCorsAndSecurity(httpSecurity);
+        configureAuth(httpSecurity);
+        return httpSecurity.build();
+    }
+    private static void configureCorsAndSecurity(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .headers(
+                        httpSecurityHeadersConfigurer ->
+                                httpSecurityHeadersConfigurer.frameOptions(
+                                        HeadersConfigurer.FrameOptionsConfig::disable
+                                )
+                )
+                // stateless한 rest api 이므로 csrf 공격 옵션 비활성화
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(HttpBasicConfigurer::disable)
+                .cors(Customizer.withDefaults())
+                .sessionManagement(configurer -> configurer
+                        .sessionCreationPolicy(
+                                SessionCreationPolicy.STATELESS
+                        )
+                );
+    }
+
+    private void configureAuth(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeHttpRequests(Customizer.withDefaults())
+                .authorizeHttpRequests(authorizeRequest -> {
+                    authorizeRequest
+                            .requestMatchers("/", "/.well-known/**", "/css/**", "/*.ico", "/error", "/images/**").permitAll()
+                            .requestMatchers(permitAllRequest()).permitAll()
+                            .requestMatchers(additionalSwaggerRequests()).permitAll()
+                            .requestMatchers(authRelatedEndpoints()).access((authentication, request) -> {
+                                String clientIp = request.getRequest().getRemoteAddr();
+                                log.info("client ip is = {}", clientIp);
+
+                                // 허용된 IP 리스트
+                                String[] allowedIps = {"127.0.0.1","172.20.10.2", "0:0:0:0:0:0:0:1"};
+
+                                // IP가 허용된 리스트에 포함되어 있는지 확인
+                                boolean isAllowed = Arrays.asList(allowedIps).contains(clientIp);
+
+                                return new AuthorizationDecision(isAllowed);
+                            });
+                });
+    }
+
+    private RequestMatcher[] permitAllRequest() {
+        List<RequestMatcher> requestMatchers = List.of(
+                antMatcher("/health_check"),
+                antMatcher("/welcome")
+        );
+        return requestMatchers.toArray(RequestMatcher[]::new);
+    }
+
+    private RequestMatcher[] additionalSwaggerRequests() {
+        List<RequestMatcher> requestMatchers = List.of(
+                antMatcher("/swagger-ui/**"),
+                antMatcher("/swagger-ui"),
+                antMatcher("/swagger-ui.html"),
+                antMatcher("/swagger/**"),
+                antMatcher("/swagger-resources/**"),
+                antMatcher("/v3/api-docs/**"),
+                antMatcher("/profile")
+        );
+        return requestMatchers.toArray(RequestMatcher[]::new);
+    }
+
+    private RequestMatcher[] authRelatedEndpoints() {
+        List<RequestMatcher> requestMatchers = List.of(
+                antMatcher("/v1/api/users")
+        );
+        return requestMatchers.toArray(RequestMatcher[]::new);
+    }
+}
