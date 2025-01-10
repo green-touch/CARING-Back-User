95fd630748aa7d2cec494d0f17a5ad096f9ea1fc
hann <wjdgks1233@gmail.com>
2025-01-10 21:37:09 +0900

    [CARE-20] feature : success build #3

--- src/main/java/com/caring/user_service/domain/user/mapper/UserMapper.java
+++ src/main/java/com/caring/user_service/domain/user/mapper/UserMapper.java
@@ -6,6 +6,10 @@ import org.mapstruct.Builder;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
+/**
+ * vo는 setter를 쓰지 않기 때문에 impl생성이 자동으로 builder가 될 수 있도록 아래와 같이 설정해줘야한다.
+ * 이때 build.gradle의 순서가 중요하니 주의
+ */
 @Mapper(componentModel = "spring", builder = @Builder(disableBuilder = false))
 public interface UserMapper {
 
