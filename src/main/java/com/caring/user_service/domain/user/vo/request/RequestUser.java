dd7dc57da1cc3af82cff07664b164233801291e4
hann <wjdgks1233@gmail.com>
2025-01-10 21:03:44 +0900

    [CARE-20] feature : create UserApiController #3

---
+++ src/main/java/com/caring/user_service/domain/user/vo/request/RequestUser.java
@@ -0,0 +1,13 @@
+package com.caring.user_service.domain.user.vo.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class RequestUser {
+    private final String password;
+    private final String name;
+}
