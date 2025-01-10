b4cb6ff7c35a3b6b6db4a6be16228b765a29b431
hann <wjdgks1233@gmail.com>
2025-01-10 20:19:29 +0900

    [CARE-20] feature : create user entity #3

---
+++ src/main/java/com/caring/user_service/domain/user/entity/User.java
@@ -0,0 +1,28 @@
+package com.caring.user_service.domain.user.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@SuperBuilder
+@EqualsAndHashCode(of = "id", callSuper = false)
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(unique = true)
+    private String userUuid;
+    @Column(unique = true)
+    private String userNumber;
+
+    private String password;
+    private String name;
+}
