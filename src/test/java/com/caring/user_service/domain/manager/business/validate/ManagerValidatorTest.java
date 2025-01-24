a6b9c7ca42fa9cb710bc94ec66e81b9d88426b13
hann <wjdgks1233@gmail.com>
2025-01-24 22:58:59 +0900

    [CARE-33] test : test ManagerValidator #5

--- src/test/java/com/caring/user_service/domain/manager/business/validate/ManagerValidatorTest.java
+++ src/test/java/com/caring/user_service/domain/manager/business/validate/ManagerValidatorTest.java
@@ -1,37 +1,48 @@
-package com.caring.user_service.domain.manager.business.service;
+package com.caring.user_service.domain.manager.business.validate;
 
 import com.caring.user_service.common.service.DatabaseCleanUp;
 import com.caring.user_service.domain.authority.business.adaptor.AuthorityAdaptor;
 import com.caring.user_service.domain.authority.entity.Authority;
 import com.caring.user_service.domain.authority.entity.ManagerRole;
+import com.caring.user_service.domain.manager.business.service.ManagerDomainService;
 import com.caring.user_service.domain.manager.entity.Manager;
 import com.caring.user_service.domain.manager.repository.ManagerRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
-
 @Slf4j
 @ActiveProfiles("test")
 @SpringBootTest
-class ManagerDomainServiceTest {
-
+class ManagerValidatorTest {
+    @Autowired
+    ManagerRepository managerRepository;
     @Autowired
     ManagerDomainService managerDomainService;
     @Autowired
-    ManagerRepository managerRepository;
+    ManagerValidator managerValidator;
     @Autowired
     AuthorityAdaptor authorityAdaptor;
     @Autowired
     DatabaseCleanUp databaseCleanUp;
+
+    Long managerId;
+
+    @BeforeEach
+    void setup() {
+        Authority authority = authorityAdaptor.queryByManagerRoleKey(ManagerRole.MANAGE.getKey());
+        managerId = managerDomainService.registerManager("name", "password", authority);
+    }
 
     @AfterEach
     void cleanUp() {
@@ -39,19 +50,16 @@ class ManagerDomainServiceTest {
     }
 
     @Test
-    @DisplayName("이름, 비밀번호, 권한을 통해서 매니저를 등록합니다.")
-    void registerManager(){
+    @Transactional
+    @DisplayName("manager가 가진 authority에 super가 포함되어있는지 확인한다.")
+    void isSuper(){
         //given
-        String name = "manager";
-        String password = "password";
-        Authority superAuthority = authorityAdaptor.queryByManagerRoleKey(ManagerRole.SUPER.getKey());
+        Manager findManager = managerRepository.findById(managerId)
+                .orElseThrow();
         //when
-        Long managerId = managerDomainService.registerManager(name, password, superAuthority);
+        boolean result = managerValidator.isSuper(findManager);
         //then
-        Optional<Manager> findManager = managerRepository.findById(managerId);
-        assertThat(findManager).isPresent();
-        assertThat(findManager.get().getName()).isEqualTo(name);
-        assertThat(findManager.get().getPassword()).isEqualTo(password);
-        log.info("findManager.memberCode = {}", findManager.get().getMemberCode());
+        assertThat(result).isFalse();
     }
+
 }
