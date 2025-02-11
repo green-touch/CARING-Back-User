88144b5d6f6a9f04a4782abd19349abe0f10576c
hann <wjdgks1233@gmail.com>
2025-02-11 20:42:05 +0900

    [CARE-46] refactor : add flow of grouping in RegisterUserByManagerUseCase #9

--- src/main/java/com/caring/user_service/domain/user/business/domainservice/UserDomainServiceImpl.java
+++ src/main/java/com/caring/user_service/domain/user/business/domainservice/UserDomainServiceImpl.java
@@ -3,6 +3,9 @@ package com.caring.user_service.domain.user.business.domainservice;
 import com.caring.user_service.common.annotation.DomainService;
 import com.caring.user_service.common.consts.StaticVariable;
 import com.caring.user_service.common.util.RandomNumberUtil;
+import com.caring.user_service.domain.shelter.entity.Shelter;
+import com.caring.user_service.domain.shelter.entity.ShelterGroup;
+import com.caring.user_service.domain.shelter.repository.ShelterGroupRepository;
 import com.caring.user_service.domain.user.entity.Role;
 import com.caring.user_service.domain.user.entity.User;
 import com.caring.user_service.domain.user.repository.UserRepository;
@@ -20,6 +23,7 @@ import static com.caring.user_service.common.util.RandomNumberUtil.generateRando
 public class UserDomainServiceImpl implements UserDomainService{
 
     private final UserRepository userRepository;
+    private final ShelterGroupRepository shelterGroupRepository;
     private final PasswordEncoder passwordEncoder;
 
     @Override
@@ -32,5 +36,14 @@ public class UserDomainServiceImpl implements UserDomainService{
                 .name(name)
                 .build();
         return userRepository.save(newUser);
+    }
+
+    @Override
+    public ShelterGroup addUserInShelterGroup(Shelter shelter, User user) {
+        ShelterGroup newShelterGroup = ShelterGroup.builder()
+                .shelter(shelter)
+                .user(user)
+                .build();
+        return shelterGroupRepository.save(newShelterGroup);
     }
 }
