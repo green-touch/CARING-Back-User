e39166aa7de9c9ae17f3c7826fb50272d664883a
KYS <ehcl1027@gmail.com>
2025-05-15 02:40:53 +0900

    [CARE-88] refactor : remove Shelter & Manager related code

--- src/main/java/com/caring/user_service/presentation/security/service/manager/ManagerTokenService.java
+++
@@ -1,6 +0,0 @@
-package com.caring.user_service.presentation.security.service.manager;
-
-import com.caring.user_service.presentation.security.service.TokenService;
-
-public interface ManagerTokenService extends TokenService {
-}
