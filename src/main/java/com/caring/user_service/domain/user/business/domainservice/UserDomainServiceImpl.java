829d787fd509ff1e2a7adddaebaf540060ed53a7
KYS <ehcl1027@gmail.com>
2025-05-19 16:54:41 +0900

    [CARE-88] feat: Add name and password validation logic to user registration

--- src/main/java/com/caring/user_service/domain/user/business/domainservice/UserDomainServiceImpl.java
+++ src/main/java/com/caring/user_service/domain/user/business/domainservice/UserDomainServiceImpl.java
@@ -6,6 +6,7 @@ import com.caring.user_service.domain.user.entity.User;
 import com.caring.user_service.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.util.StringUtils;
 
 import java.util.UUID;
 
@@ -14,13 +15,16 @@ import static com.caring.user_service.common.util.RandomNumberUtil.generateRando
 
 @DomainService
 @RequiredArgsConstructor
-public class UserDomainServiceImpl implements UserDomainService{
+public class UserDomainServiceImpl implements UserDomainService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
     @Override
     public User registerUser(String name, String password) {
+        validateName(name);
+        validatePassword(password);
+
         User newUser = User.builder()
                 .memberCode(generateRandomMemberCode(USER_MEMBER_CODE_PRESET))
                 .userUuid(UUID.randomUUID().toString())
@@ -31,4 +35,21 @@ public class UserDomainServiceImpl implements UserDomainService{
         return userRepository.save(newUser);
     }
 
+    private void validateName(String name) {
+        if (name == null) {
+            throw new IllegalArgumentException("이름은 null일 수 없습니다");
+        }
+        if (!StringUtils.hasText(name)) {
+            throw new IllegalArgumentException("이름은 빈 문자열일 수 없습니다");
+        }
+    }
+
+    private void validatePassword(String password) {
+        if (password == null) {
+            throw new IllegalArgumentException("비밀번호는 null일 수 없습니다");
+        }
+        if (!StringUtils.hasText(password)) {
+            throw new IllegalArgumentException("비밀번호는 빈 문자열일 수 없습니다");
+        }
+    }
 }
