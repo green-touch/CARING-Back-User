5f69a2d97bef226fa95b1159c60704b10a481478
KYS <ehcl1027@gmail.com>
2025-05-29 18:17:24 +0900

    [CARE-88] refactor: remove ManagerRole

--- src/main/java/com/caring/user_service/presentation/user/controller/UserApiController.java
+++ src/main/java/com/caring/user_service/presentation/user/controller/UserApiController.java
@@ -1,8 +1,6 @@
 package com.caring.user_service.presentation.user.controller;
 
-import com.caring.user_service.common.annotation.ManagerRoles;
-import com.caring.user_service.common.consts.StaticVariable;
-import com.caring.user_service.common.util.RoleUtil;
+import com.caring.user_service.common.annotation.ManagerCode;
 import com.caring.user_service.presentation.user.service.GetUserProfileUseCase;
 import com.caring.user_service.presentation.user.service.GetUserShelterUuidUseCase;
 import com.caring.user_service.presentation.user.vo.ResponseUser;
@@ -15,8 +13,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @Tag(name = "[회원(AUTH)]")
 @Slf4j
@@ -31,16 +27,14 @@ public class UserApiController {
     @Operation(summary = "특정 유저의 계정을 조회합니다.")
     @GetMapping("/{userUuid}")
     public ResponseUser getUserProfile(@PathVariable String userUuid,
-            @ManagerRoles List<String> roles) {
-        RoleUtil.containManagerRole(StaticVariable.ALL_MANAGER_ACCESS, roles);
+            @ManagerCode String managerCode) {
         return getUserProfileUseCase.execute(userUuid);
     }
 
     @Operation(summary = "특정 유저의 shelterUuid를 반환합니다. 유저가 없으면 404를 반환합니다.")
     @GetMapping("/{userUuid}/shelterUuid")
     public ResponseUserShelterUuid getUserShelterUuid(@PathVariable String userUuid,
-            @ManagerRoles List<String> roles) {
-        RoleUtil.containManagerRole(StaticVariable.ALL_MANAGER_ACCESS, roles);
+            @ManagerCode String managerCode) {
         return getUserShelterUuidUseCase.execute(userUuid);
     }
 
